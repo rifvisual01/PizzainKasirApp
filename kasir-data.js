a9923565bef1f174bb4daff31523c0df51dba0b2
rifvisual01 <rifvisual01@gmail.com>
2026-01-21 00:03:22 +0700

    Create kasir-data.js

---
+++ kasir-data.js
@@ -0,0 +1,27 @@
+// ================================
+// DATA KASIR LOKAL (OFFLINE-FIRST)
+// ================================
+window.LOCAL_CASHIERS = [
+  {
+    name: 'Rifqi',
+    pin: '1111',
+    active: true,
+    role: 'cashier'
+  }
+
+   {
+    name: 'Afifah',
+    pin: '2222',
+    active: true,
+    role: 'cashier'
+  }
+
+ {
+    name: 'iki',
+    pin: '3333',
+    active: true,
+    role: 'cashier'
+  }
+
+
+];
