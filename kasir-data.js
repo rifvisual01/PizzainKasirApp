7412287e76534b397f2478d70f1949d550e4b556
rifvisual01 <rifvisual01@gmail.com>
2026-01-25 18:21:45 +0700

    Update kasir-data.js

--- kasir-data.js
+++ kasir-data.js
@@ -3,6 +3,14 @@ window.LOCAL_CASHIERS = [
   {
     name: 'Rifqi',
     pin: '1111',
+    role: 'kasir',
+    active: true
+  },
+  {
+    name: 'Admin',
+    pin: '9999',
+    role: 'admin',
     active: true
   }
 ];
+
