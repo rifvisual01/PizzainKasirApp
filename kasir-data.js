4a52736caaeb7063e82b4e8c31be55f91eb558ea
rifvisual01 <rifvisual01@gmail.com>
2026-08-30 01:10:44 +0700

    Add files via upload

--- kasir-data.js
+++ kasir-data.js
@@ -1,8 +1,8 @@
 // kasir-data.js
 window.LOCAL_CASHIERS = [
   {
-    name: 'Arzha',
-    pin: '190405',
+    name: 'Tifani',
+    pin: '040523',
     role: 'kasir',
     active: true
   },
@@ -15,15 +15,28 @@ window.LOCAL_CASHIERS = [
   },
   
    {
-    name: 'Afdhal',
-    pin: '246810',
+    name: 'Sarah',
+    pin: '130419',
     role: 'kasir',
     active: true
   },
   {
     name: 'Admin',
-    pin: '9999',
+    pin: '0000',
     role: 'admin',
     active: true
+  },
+   {
+    name: 'Arzha',
+    pin: '190405',
+    role: 'kasir',
+    active: true
+  },
+   {
+    name: 'Afdhal',
+    pin: '246810',
+    role: 'kasir',
+    active: true
   }
+
 ];
